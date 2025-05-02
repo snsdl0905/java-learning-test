09f8e5f8324a2399a3935b0d1b13ab719a6814a1
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2025-05-02 16:01:02 +0900

    🚀3단계 - 문자열 계산기 구현

---
+++ java-test/initial/src/test/java/calculator/StringCalculator.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    private static final String DELIMITER = "[,|:]";
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.*?)\\n(.*)");
+
+    public int calculate(String input){
+        String delimiter = DELIMITER;
+        String numbers = input;
+
+        Matcher matcher = CUSTOM_PATTERN.matcher(input);
+
+        if(matcher.find()){
+            delimiter += '|' + Pattern.quote(matcher.group(1));
+            numbers = matcher.group(2);
+        }
+
+        String[] numberArray = numbers.split(delimiter);
+        return addNumbers(numberArray);
+    }
+
+    private int addNumbers(String[] numberArray){
+        return Arrays.stream(numberArray)
+            .map(String::trim)
+            .mapToInt(this::validateNumber)
+            .sum();
+    }
+
+    public int validateNumber(String number){
+        try {
+            int num = Integer.parseInt(number);
+            if (num < 0) {
+                throw new RuntimeException("음수는 허용되지 않습니다: " + num);
+            }
+            return num;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("숫자가 아닌 값이 포함되어 있습니다: " + number);
+        }
+    }
+}
--- java-test/initial/src/test/java/test/CalculatorTest.java
+++ java-test/initial/src/test/java/test/CalculatorTest.java
@@ -1,3 +1,6 @@
+package test;
+
+import calculator.Calculator;
 import org.junit.jupiter.api.Test;
 import static  org.junit.jupiter.api.Assertions.*;
 
